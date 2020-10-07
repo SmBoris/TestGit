4f88077b6b148873d2a043be794cfc8c98e8075c
boris <berserkeerr@gmail.com>
2020-10-07 20:15:19 +0300

    Update3

--- src/FirstGit.java
+++ src/FirstGit.java
@@ -2,5 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Privet Medved");
         System.out.println("Update");
+        System.out.println("Tolls");
     }
 }
